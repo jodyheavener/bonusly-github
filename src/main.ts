b1ffa3ec8a8857052a98b699bcc574cbfbe4dece
Jody Heavener <j.heavener@gmail.com>
2020-09-02 22:01:16 -0400

    asff

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import * as core from '@actions/core'
 async function run(): Promise<void> {
   try {
     core.setSecret('api-token')
+    core.info('heyuyyyyyyyyy')
 
     const apiToken: string = core.getInput('api-token')
     core.debug(`API Token: ${apiToken}`)
